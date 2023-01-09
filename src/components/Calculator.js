4a2eba619f1bbca4eafee77f2835218b70e97b76
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-09 21:58:33 +0530

    styling is ready, some modification still needed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,29 +4,29 @@ import './Calculator.css';
 const Calculator = () => {
   return (
     <div className='' style={{backgroundColor: ''}}>
-      <div className="container mt-5" style={{ backgroundColor: 'orange', maxWidth: '25em' }}>
+      <div className="container mt-5 rounded" style={{ backgroundColor: 'red', maxWidth: '35em' }}>
         <div className="row">
           <div id='display' className="col-12 col-md-12">{'A'}</div>
         </div>
-        <div className="row">
-          <div className="col col-6 col-md-6 pt-3 pb-3">AC</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3 operators">/</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3 operators">x</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">7</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">8</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">9</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3 operators">-</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">4</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">5</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">6</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3 operators">+</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">1</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">2</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">3</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">=</div>
-          <div className="col col-6 col-md-6 pt-3 pb-3">0</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">.</div>
-          <div className="col col-3 col-md-3 pt-3 pb-3">🔥</div>
+        <div className="row p-2">
+          <div  className="col col-6 col-md-6 border pt-3 pb-3">AC</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3 operators">/</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3 operators">x</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">7</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">8</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">9</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3 operators">-</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">4</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">5</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">6</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3 operators">+</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">1</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">2</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">3</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">=</div>
+          <div  className="col col-6 col-md-6 border pt-3 pb-3">0</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">.</div>
+          <div  className="col col-3 col-md-3 border pt-3 pb-3">🔥</div>
         </div>
       </div>
     </div>
